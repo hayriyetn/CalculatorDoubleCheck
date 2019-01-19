1fe777fb8a9bf2d1ce43b077f3c9465f851f78da
Hayriye <hayriyetn@gmail.com>
2019-01-19 12:36:39 -0600

    with multiply

--- src/newnew.java
+++ src/newnew.java
@@ -2,4 +2,8 @@ public class newnew {
     public static double add (double num1,double num2){
         return num1+num2;
     }
+    public static double multiply (double num1,double num2){
+        double sum=num1*num2;
+        return sum;
+    }
 }
